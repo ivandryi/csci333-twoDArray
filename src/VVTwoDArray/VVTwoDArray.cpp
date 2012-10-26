d51f8fef1c7cf5336bbb26bbf60f8ecc269c1272
Iryna <iryna@debian.com>
2012-10-26 17:14:06 -0400

    modified vector of vectors

--- src/VVTwoDArray/VVTwoDArray.cpp
+++ src/VVTwoDArray/VVTwoDArray.cpp
@@ -3,29 +3,50 @@
 #include <assert.h>
 
 VVTwoDArray::VVTwoDArray(int r, int c, int def) {
-
+  assert (r > 0 && c > 0);
+  defaultValue = def;
+  row = r;
+  column = c;
+  theArray.resize(row);
+  for (int i = 0; i < row; i++) {
+    theArray[i].resize(column);
+  }
+  for (int i = 0; i < row; i++)
+    for (int j = 0; j < column; j++)
+      theArray[i][j] = defaultValue;
 }
 
 VVTwoDArray::~VVTwoDArray() {
-
+  std::cout << "Destructor call" << std::endl;
 }
 
-void VVTwoDArray::access(int r, int c) {
+void VVTwoDArray::insert(int r, int c, int val) {
+  assert (r > 0 && c > 0 && r <= row && c <= column);
+  theArray[r - 1][c - 1] = val;
+}
 
+int  VVTwoDArray::access(int r, int c) {
+  assert(r > 0 && c > 0 && r <= row && c <= column);
+  return theArray[r - 1][c - 1];
 }
 
 void VVTwoDArray::remove(int r, int c) {
-
+  assert (r > 0 && c > 0 && r <=row && c <= column);
+  theArray[r - 1][c - 1] = defaultValue;
 }
 
 void VVTwoDArray::print() {
-
+  for (int i = 0; i < row; i++) {
+    std::cout << "\n" << std::endl;
+    for (int j = 0; j < column; j++)
+      std::cout << theArray[i][j];
+  }
 }
 
 int VVTwoDArray::getNumRows() {
-
+  return row;
 }
 
 int VVTwoDArray::getNumCols() {
-
+  return column;
 }
--- test/Test.cpp
+++ test/Test.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include "../src/TwoDArray/TwoDArray.h"
-#include "../src/VVTwoDArray/VVTwoDArray.h"
-#include "../src/STwoDArray/STwoDArray.h"
+//#include "../src/VVTwoDArray/VVTwoDArray.h"
+//#include "../src/STwoDArray/STwoDArray.h"
 
+TEST(TwoDArray, CreateDestroy) {
+  TwoDArray* array = new TwoDArray (5, 3, 0);
+  delete array;
+}
 
